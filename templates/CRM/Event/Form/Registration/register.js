1dbb76f8c609423073e24cd268bfb7d05cbca9a4
Erawat Chamanont <erawat@gmail.com>
2013-10-15 15:10:03 +0100

    fixed the js

--- templates/CRM/Event/Form/Registration/register.js
+++ templates/CRM/Event/Form/Registration/register.js
@@ -1,9 +1,7 @@
 cj(function($){
-  //$('#noOfparticipants').hide();
   $('input[type=radio]').on('change', function() {
     if($(this).attr('price')){
       eval( 'var option = ' + $(this).attr('price') );
-      ele        = option[0];
       var optionPart = option[1].split(optionSep);
       var participants = optionPart[1] - 1;
       $('#additional_participants').val(participants);

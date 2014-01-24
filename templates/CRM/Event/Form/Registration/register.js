a66fa5a7487f849b630ee58a7c51a314d91c67e5
Erawat Chamanont <erawat@gmail.com>
2014-01-24 17:27:12 +0000

    if the discount code is entered, don’t disable only additional participant field 0

--- templates/CRM/Event/Form/Registration/register.js
+++ templates/CRM/Event/Form/Registration/register.js
@@ -1,5 +1,6 @@
 cj(function($){
 
+
   $('input[type=radio]').on('change', function() {
     if($(this).attr('price')){
       eval( 'var option = ' + $(this).attr('price') );
@@ -9,12 +10,19 @@ cj(function($){
       }
   });
 
-
+  var discountcode = $('#discountcode').val();
   $('input[type=radio]').each(function(){
     var el  = $(this);
     if($(el).attr('name') != 'payment_processor'){
-      if(el.data('amount') == 0){
-        el.parent().parent().hide();
+      if(discountcode.length > 0){ //check if discount code is empty;
+         var text = $('label[for='+ el.attr('id') +']').text();
+        if(text.indexOf('Additional participant') !== -1){
+          el.parent().parent().hide();
+        }
+      }else{
+        if(el.data('amount') == 0){
+          el.parent().parent().hide();
+        }
       }
       if(el.attr('value') == 0){
         el.prop('checked', true);
